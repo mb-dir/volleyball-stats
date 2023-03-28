d7bb34090b6c5f35a91306c5d810c718ff908475
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-03-28 14:42:17 +0200

    Create Layout component with menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
+import Clubs from "./components/Clubs/Clubs";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />} />
+      <Route path="/" element={<Layout />}>
+        <Route path="kluby" element={<Clubs />} />
+      </Route>
     </Routes>
   );
 }
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,22 @@
 import { Outlet } from "react-router-dom";
+import Menu from "../Menu/Menu";
+import Header from "../Header/Header";
+import "./layout.css";
 
 interface LayoutProps {}
 
 const Layout: React.FC<LayoutProps> = () => {
   return (
     <main className="App">
-      menu
+      <Header />
+      <Menu
+        items={[
+          { label: "Tabela", onClick: () => console.log("Item 1 clicked") },
+          { label: "Mecze", onClick: () => console.log("Item 2 clicked") },
+          { label: "Kluby", onClick: () => console.log("Item 3 clicked") },
+          { label: "Zawodnicy", onClick: () => console.log("Item 3 clicked") },
+        ]}
+      />
       <Outlet />
     </main>
   );
