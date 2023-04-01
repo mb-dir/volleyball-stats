185a18617492239b56009ffbb92d1eb3cec37fee
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-04-01 23:36:12 +0200

    Create axios instance

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,22 @@ import Table from "./components/Table/Table";
 import Games from "./components/Games/Games";
 import Players from "./components/Players/Players";
 import NotFound from "./components/NotFound/NotFound";
+import axios from "./api/axios";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const response = await axios.get("/leagues");
+        console.log(response.data.response);
+        return response.data;
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getUsers();
+  }, []);
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
