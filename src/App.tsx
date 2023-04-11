dde8c11055c048a9ec99ca85eccf39d83c0a7fb6
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-04-11 22:30:16 +0200

    Create main view

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import Table from "./components/Table/Table";
 import Games from "./components/Games/Games";
 import NotFound from "./components/NotFound/NotFound";
 import ClubView from "./components/Clubs/ClubView";
+import Main from "./components/Main/Main";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+        <Route index element={<Main />} />
         <Route path="kluby">
           <Route index element={<Clubs />} />
           <Route path=":id" element={<ClubView />} />
